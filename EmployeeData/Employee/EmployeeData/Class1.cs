699ae497f8b61b7cf2a2c64b97bc98513d2ef839
Ankan Mondal <ankan.mondal@automationanywhere.com>
2019-08-11 13:03:50 +0530

    Pushing Employee Data

---
+++ EmployeeData/Employee/EmployeeData/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+
+namespace EmployeeData
+{
+    public class EmployeeClass
+    {
+        public string EmployeeData(string URL)
+        {
+            using (var httpclient = new HttpClient())
+            {
+                var res = httpclient.GetStringAsync(new Uri(URL)).Result;
+                return res;
+
+            }
+        }
+    }
+}
---
+++ EmployeeData/Employee/EmployeeTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EmployeeData;
+
+namespace EmployeeTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeClass em = new EmployeeClass();
+            Console.WriteLine(em.EmployeeData("https://hooks.slack.com/services/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"));
+            Console.ReadLine();
+        }
+    }
+}
